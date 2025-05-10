e191ed25952bb700bf6a4c702f7ccf7c27f335b9
Iann <93653677+GameSa0@users.noreply.github.com>
2025-05-10 14:12:37 +0800

    updated

---
+++ locustfile.py
@@ -0,0 +1,38 @@
+from locust import HttpUser, task, between
+import random
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3)
+    user_counter = 0  
+
+    def on_start(self):
+        WebsiteUser.user_counter += 1
+        self.user_id = WebsiteUser.user_counter
+        self.username = f"user{self.user_id}"
+        self.password = "test123"
+        self.session = self.client
+
+        self.session.post("/register", data={
+            "username": self.username,
+            "password": self.password
+        })
+
+        self.session.post("/login", data={
+            "username": self.username,
+            "password": self.password
+        })
+
+    @task
+    def visit_home_and_add_info(self):
+        self.session.get("/home")
+
+        self.session.post("/add-info", data={
+            "fname": "Cesar Ian ",
+            "mname": "C.",
+            "lname": "Apatino",
+            "age": str(random.randint(18, 60)),
+            "address": "Butuan City",
+            "birthday": "2001-09-15"
+        })
+
+        print(f"user {self.user_id}: Done queue")
